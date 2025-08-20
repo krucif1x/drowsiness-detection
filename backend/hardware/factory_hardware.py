c3deab3f36e451ffac2af8481fa55bb77e07e3ff
krucif1x <119868033+krucif1x@users.noreply.github.com>
2025-08-20 11:56:43 +0700

    Update factory_hardware.py

--- backend/hardware/factory_hardware.py
+++ backend/hardware/factory_hardware.py
@@ -1,16 +1,16 @@
-import os
+import os #Provides tools to interact with the OS, such as findoing out its name
 
 
-def get_camera():
-    if os.name == "posix":
-        from backend.hardware.camera.rpi_camera import RPiCamera
-        return RPiCamera()
-    from backend.hardware.camera.cv_camera import CVCamera
-    return CVCamera()
+def get_camera(): #figure out which camera class to use, create an instance of it, and return it
+    if os.name == "posix": #checks the name attribute of the os module
+        from backend.hardware.camera.rpi_camera import RPiCamera #If True then import RPICamera class
+        return RPiCamera() #Creates insatcne of RPICamera class
+    from backend.hardware.camera.cv_camera import CVCamera #It imports the CVCamera class
+    return CVCamera()#Creates instacne of CVCamera
 
 def get_buzzer():
     if os.name == "posix":
-        from backend.hardware.buzzer.rpi_buzzer import RaspberryBuzzer
-        return RaspberryBuzzer()
+        from backend.hardware.buzzer.rpi_buzzer import RaspberryBuzzer#It imports the RaspberryBuzzer class
+        return RaspberryBuzzer() #returns an instance of the RaspberryBuzzer class, which controls the physical GPIO pins
     from backend.hardware.buzzer.windows_buzzer import WindowsBuzzer
-    return WindowsBuzzer()
+    return WindowsBuzzer() # imports and returns an instance of the WindowsBuzzer class, which uses the winsound library to make sounds through the computer's speakers
