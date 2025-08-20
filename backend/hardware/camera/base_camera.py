6b69108aed53e8e468e0fade5d5dcfe1b03ba216
krucif1x <119868033+krucif1x@users.noreply.github.com>
2025-08-20 11:28:06 +0700

    Update base_camera.py

--- backend/hardware/camera/base_camera.py
+++ backend/hardware/camera/base_camera.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class BaseCamera(ABC):
     @abstractmethod
-    def get_capture(self) -> tuple[bool, np.ndarray]:
+    def get_capture(self) -> tuple[bool, np.ndarray]: #This is a type hint. It specifies that this method must return a tuple containing two elements: a boolean value (bool) and a NumPy array (np.ndarray)
         """
         Returns a tuple of (ret, frame) just like OpenCV,
         where `ret` is a boolean and `frame` is a numpy ndarray (or None).
