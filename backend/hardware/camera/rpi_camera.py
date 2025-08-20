8386395200b5729a495f05e334cf8f6f112f5244
krucif1x <119868033+krucif1x@users.noreply.github.com>
2025-08-20 11:49:12 +0700

    Update rpi_camera.py

--- backend/hardware/camera/rpi_camera.py
+++ backend/hardware/camera/rpi_camera.py
@@ -1,19 +1,19 @@
-import numpy as np
-from picamera2 import Picamera2
+import numpy as np #Essential because camera frame will be provieded as a NumPy array
+from picamera2 import Picamera2 #Primary tool for interfacing with Raspi Cam
 
 from backend.hardware.camera.base_camera import BaseCamera
 from backend.utils.logging import logging_default
 
 
-class RPiCamera(BaseCamera):
+class RPiCamera(BaseCamera): #creates a new class named RPiCamera that inherits from BaseCamera
     def __init__(self):
         logging_default.info("Setting up the camera")
-        self.picam2 = Picamera2()
-        self.picam2.start()
+        self.picam2 = Picamera2() #creates an instance of the Picamera2 class, initializes the connection to the physical camera hardware
+        self.picam2.start() # calls the .start() method on the camera object. This powers up the camera sensor and starts the video stream
 
-    def get_capture(self) -> np.ndarray:
-        try:
-            frame = self.picam2.capture_array()
+    def get_capture(self) -> np.ndarray: #purpose is to capture a single, current frame from the camera's video stream
+        try: #Error-Handling Mechanism
+            frame = self.picam2.capture_array() #calls the .capture_array() method on the picam2 object, which grabs the latest frame from the video stream and returns it as a NumPy array
             if frame is not None:
                 return True, frame
             logging_default.warning("PiCamera2 returned None")
